c140ce18ecf04ec5ae50dac6a08a236173cd647b
anjanchara0 <anjanchara0@gmail.com>
2026-09-08 14:21:55 +0530

    bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,63 @@
-Done! Congratulations on your new bot. You will find it at t.me/Prashantsldbot. You can now add a description, about section and profile picture for your bot, see /help for a list of commands. By the way, when you've finished creating your cool bot, ping our Bot Support if you want a better username for it. Just make sure the bot is fully operational before you do this.
+import os
+import telebot
+from telebot import apihelper
+import yt_dlp
 
-Use this token to access the HTTP API:
-8971427857:AAEaGfBJ3OzIM4j3_uPWLzbZDXwE1MUTZWQ
-Keep your token secure and store it safely, it can be used by anyone to control your bot.
+apihelper.CONNECT_TIMEOUT = 300
+apihelper.READ_TIMEOUT = 300
 
-For a description of the Bot API, see this page: https://core.telegram.org/bots/api
+# Apna bot token quotes ke andar dalein
+BOT_TOKEN = "8971427857:AAEaGfBJ3OzIM4j3_uPWLzbZDXwE1MUTZWQ"
+
+bot = telebot.TeleBot(BOT_TOKEN)
+
+
+@bot.message_handler(commands=['start'])
+def send_welcome(m):
+  bot.reply_to(
+      m, 'Namaste! Mujhe video link bhejein, main turant download karke dunga.'
+  )
+
+
+@bot.message_handler(func=lambda m: True)
+def dl(m):
+  url = m.text.strip()
+  if not url.startswith('http'):
+    bot.reply_to(m, 'Kripya sahi link bhejein.')
+    return
+
+  msg = bot.reply_to(m, '⚡ Downloading...')
+
+  # Fast download aur chhota size (Mobile optimized)
+  opts = {
+      'format': 'bestvideo[height<=720][ext=mp4]+bestaudio[ext=m4a]/best[height<=720]/best',
+      'outtmpl': 'fast_vid.%(ext)s',
+      'quiet': True,
+      'no_warnings': True,
+  }
+
+  try:
+    with yt_dlp.YoutubeDL(opts) as ydl:
+      info = ydl.extract_info(url, download=True)
+      fn = ydl.prepare_filename(info)
+
+    bot.edit_message_text(
+        '🚀 Uploading to Telegram...', m.chat.id, msg.message_id
+    )
+
+    with open(fn, 'rb') as vf:
+      bot.send_video(m.chat.id, vf, timeout=300)
+
+    if os.path.exists(fn):
+      os.remove(fn)
+    bot.delete_message(m.chat.id, msg.message_id)
+
+  except Exception as e:
+    print('Error:', e)
+    bot.edit_message_text(
+        f'Dikkat aayi: {str(e)[:100]}', m.chat.id, msg.message_id
+    )
+
+
+print('Bot chalu ho gaya hai!')
+bot.infinity_polling(timeout=60, long_polling_timeout=60)
