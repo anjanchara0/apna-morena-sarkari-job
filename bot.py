fcc50c40e54d6e14fb7ca026e862f1f170e2d51e
anjanchara0 <anjanchara0@gmail.com>
2026-09-09 08:39:03 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -37,17 +37,21 @@ def dl(m):
     out_tmpl = f"dl_{m.chat.id}_{m.message_id}_%(id)s.%(ext)s"
 
     opts = {
-        'format': 'b[acodec!=none][vcodec!=none]/best[acodec!=none][vcodec!=none]/best',
+        'format': 'b[ext=mp4]/best[ext=mp4]/b/best',
         'outtmpl': out_tmpl,
         'quiet': True,
         'no_warnings': True,
         'extractor_args': {
             'youtube': {
-                'player_client': ['android_embedded', 'android'],
+                'player_client': ['web_embedded', 'android_creator'],
+                'player_skip': ['webpage', 'configs']
             }
+        },
+        'http_headers': {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36',
+            'Referer': 'https://www.google.com/'
         }
     }
-
     try:
         with yt_dlp.YoutubeDL(opts) as ydl:
             ydl.download([url])
