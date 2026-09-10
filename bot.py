df4828e96b9ef897e598810daf699fe5665d091a
anjanchara0 <anjanchara0@gmail.com>
2026-09-10 12:52:55 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -18,7 +18,7 @@ from reportlab.lib import colors
 # ==========================================
 # 1. कॉन्फ़िगरेशन
 # ==========================================
-BOT_TOKEN = "8526721171:AAEN1zSLW1DkNqf6EaZwDFFW5-bcfvkTa6M"
+BOT_TOKEN = "8526721171:AAEJy5j04q3zB-rBcywxwxIjyKFGVzef24Q"
 CHANNEL_ID = "@apnamorenasarkarijob"
 WEBHOOK_URL = "https://apna-morena-sarkari-job.onrender.com"
 
