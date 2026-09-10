98641576708d29dd34f040586e75fcde3ae492ec
anjanchara0 <anjanchara0@gmail.com>
2026-09-10 16:02:59 +0530

    Correct BOT_TOKEN and webhook URL formatting
    Fix formatting of BOT_TOKEN assignment and update webhook URL.

--- bot.py
+++ bot.py
@@ -19,7 +19,7 @@ from reportlab.lib import colors
 # ==========================================
 # 1. कॉन्फ़िगरेशन
 # ==========================================
-BOT_TOKEN = os.environ.get("BOT_TOKEN", ("8526721171:AAHI55V6vyEaMvG0cBRXrjCnorPgDDC5eZg")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "8526721171:AAHI55V6vyEaMvG0cBRXrjCnorPgDDC5eZg")
 CHANNEL_ID = "@apnamorenasarkarijob"
 WEBHOOK_URL = "https://apna-morena-sarkaupdate = telebot.types.Update.de_json(json_string)ri-job.onrender.com"
 
